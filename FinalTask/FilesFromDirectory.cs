348df22de7b55608edb959a084bed14ac31cd663
Lukasz Urbanski <lukasz.urbanski@yahoo.pl>
2019-07-21 16:47:55 +0200

    Sunday EOB

---
+++ FinalTask/FilesFromDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FinalTask
+{
+    static class FilesFromDirectory
+    {
+        public static List<FileInfo> GetAllFilesFromDirectory(string path)
+        {
+            List<FileInfo> fileInfos = new List<FileInfo>();
+            string[]allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (var files in allfiles)
+            {
+                fileInfos.Add(new FileInfo(files));
+            }
+            return fileInfos;
+        }
+    }
+}
--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
 
 namespace FinalTask
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            XNamespace xNamesapce = "urn:oasis:names:tc:xliff:document:1.2";
+            Console.WriteLine("Path: ");
+            string path = @"c:\Users\lurbanski\Desktop\Szkola\TMS Archive replacer inputs\FixedBilingualFiles\";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            FileInfo[] files = directoryInfo.GetFiles("*.sdlxliff", SearchOption.AllDirectories);
+
+            XDocument document = XDocument.Load("docx.xml");
+
+            string value = document.Root.Element(xNamesapce + "file").Attribute("source-language").Value;
+            Console.WriteLine(value);
+            Console.ReadKey();
         }
     }
 }
